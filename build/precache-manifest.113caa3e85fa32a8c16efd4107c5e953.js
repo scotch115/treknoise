721506860f4cca8b064c0e6d4ebf482f2be0ebb7
scotch115 <jordangamache115@gmail.com>
2020-02-23 10:12:31 -0500

    Adding home screen icon with correct background color

---
+++ build/precache-manifest.113caa3e85fa32a8c16efd4107c5e953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906cacf0d7921b05cbee0be86907c9d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bbcd5b93ca3d7e30046",
+    "url": "/static/css/main.7f414a0c.chunk.css"
+  },
+  {
+    "revision": "5e40d8fd6dcba4168d77",
+    "url": "/static/js/2.3819856b.chunk.js"
+  },
+  {
+    "revision": "3e4d2f898c5f846709bbafd166d6d90b",
+    "url": "/static/js/2.3819856b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbcd5b93ca3d7e30046",
+    "url": "/static/js/main.b93321fd.chunk.js"
+  },
+  {
+    "revision": "8387666c7fa98fa9b3da",
+    "url": "/static/js/runtime-main.5114592b.js"
+  }
+]);
